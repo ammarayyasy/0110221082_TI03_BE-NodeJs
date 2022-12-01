6c69950828e3c9e9d68128e7f19f703f2f58de12
unknown <ammarmojur@gmail.com>
2022-12-01 23:17:11 +0700

    FruitController

---
+++ Controllers/FruitController.js
@@ -0,0 +1,69 @@
+/**
+ * TODO 3:
+ * - import fruits dari data/fruits.js
+ * - refactor variabel ke ES6 variable
+ */
+const fruits = require("../data/fruits.js");
+
+/**
+ * TODO 4:
+ * - Buat method index.
+ * - Refactor function ke ES6 Arrow Function
+ * - Tampilkan data fruits.
+ *
+ * @hint - Gunakan looping for of
+ */
+const index = () => {
+  for (const fruit of fruits) {
+    console.log(`- ${fruit}`);
+  }
+};
+
+/**
+ * TODO 5:
+ * - Buat method store.
+ * - Refactor function ke ES6 Arrow Function
+ * - Menambahkan data baru ke array fruits.
+ *
+ * @param {string} name - Nama buah.
+ *
+ * @hint - Gunakan method push
+ */
+const store = (name) => {
+  fruits.push(name);
+  index();
+};
+
+/**
+ * TODO 6:
+ * - Buat method update.
+ * - Refactor function ke ES6 Arrow Function
+ * - Memperbarui data fruits.
+ *
+ * @param {number} position - Posisi atau index yang ingin diupdate.
+ * @param {string} name - Nama buah yang baru.
+ */
+const update = (position, name) => {
+  fruits[position] = name;
+  index();
+};
+
+/**
+ * TODO 7:
+ * - Buat method destroy.
+ * - Refactor function ke ES6 Arrow Function
+ * - Menghapus data fruits.
+ *
+ * @param {number} position - Posisi atau index yang ingin dihapus
+ *
+ * @hint - Gunakan method splice
+ */
+const destroy = (position) => {
+  fruits.splice(position, 1);
+  index();
+};
+
+/**
+ * TODO 8: export method index, store, update, dan destroy
+ */
+module.exports = { index, store, update, destroy };
---
+++ data/fruits.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+const fruits = ["Melon", "Apel", "Jeruk"];
+
+// TODO 2: export variable fruits
+module.exports = fruits;
